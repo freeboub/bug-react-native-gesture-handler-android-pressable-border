09fbf5ec282dd648e72da2680c187e3d95ed824c
Olivier Bouillet <olivier@azzapp.com>
2024-11-29 10:47:40 +0100

    chore: show ripple color issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text, View } from 'react-native';
+import {GestureHandlerRootView, Pressable} from 'react-native-gesture-handler'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+const componentList: number[] = Array.from(new Array(100).keys());
 
+  
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
@@ -27,13 +28,18 @@ export default function RootLayout() {
     return null;
   }
 
+  const rippleColor = '#FF0000';
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView>
+      <View style={{flex: 1, backgroundColor: '#ABABAB4F'}}>
+          <Pressable
+              onPress={() => console.log('onPress')}
+              android_ripple={{color: rippleColor}}
+          >
+            <Text>'Hello World'</Text>
+        </Pressable>
+      </View>
+    </GestureHandlerRootView>
   );
 }
