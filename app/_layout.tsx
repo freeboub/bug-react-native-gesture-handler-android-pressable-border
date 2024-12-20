6caae5d356b42ef1c08e084c00402bc50f905e6c
Olivier Bouillet <olivier@azzapp.com>
2024-12-20 19:34:50 +0100

    fix: upgrade sample

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,27 @@
-import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 import {GestureHandlerRootView, Pressable} from 'react-native-gesture-handler'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-const componentList: number[] = Array.from(new Array(100).keys());
 
-  
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
   useEffect(() => {
-    if (loaded) {
       SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  const rippleColor = '#FF0000';
+  }, []);
 
   return (
     <GestureHandlerRootView>
-      <View style={{flex: 1, backgroundColor: '#ABABAB4F'}}>
-          <Pressable
-              onPress={() => console.log('onPress')}
-              android_ripple={{color: rippleColor}}
-          >
-            <Text>'Hello World'</Text>
-        </Pressable>
-      </View>
+      <Pressable
+          style={{borderBottomColor: '#FF0000', borderBottomWidth: 5}}
+          onPress={() => console.log('onPress')}
+      >
+        <Text>'Hello World'</Text>
+      </Pressable>
     </GestureHandlerRootView>
   );
 }
